dbd4a8d73124adb0e4e1ef0c97aa49ce0b90da46
yuran1811 <trieuvanbd123@gmail.com>
2021-11-06 19:49:32 +0700

    Add New File

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
+
+sign_up_btn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode');
+});
+
+document.querySelector('.log-in').addEventListener('click', (e) => {
+  const userName = document.querySelector('.user-name').value;
+  const Password = document.querySelector('.password').value;
+  if (userName !== 'Admin' || Password !== '1234')
+    alert('You are not Admin, please try again!');
+  else {
+    alert('Log In Successful');
+    sign_up_btn.classList.add('active');
+    $('#sign-in-btn a').attr('href', 'http://www.google.com/');
+  }
+});
