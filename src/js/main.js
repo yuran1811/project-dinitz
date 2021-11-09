919512f31e3419c12f87d3a26ebe9ac04d05a64e
yuran1811 <trieuvanbd123@gmail.com>
2021-11-09 07:52:17 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,20 @@ sign_up_btn.addEventListener('click', () => {
   container.classList.add('sign-up-mode');
 });
 
-document.querySelector('.log-in').addEventListener('click', (e) => {
+function LogIn() {
   const userName = document.querySelector('.user-name').value;
   const Password = document.querySelector('.password').value;
-  if (userName !== 'Admin' || Password !== '1234')
-    alert('You are not Admin, please try again!');
+  if (userName !== 'Admin' || Password !== '1234') return;
+  // alert('You are not Admin, please try again!');
   else {
     alert('Log In Successful');
     sign_up_btn.classList.add('active');
     $('#sign-in-btn a').attr('href', 'src/Test_Template.html');
   }
+}
+
+document.querySelector('.log-in').addEventListener('click', LogIn());
+
+$('.input-field').on('keydown', function (event) {
+  if (event.keyCode === 13) LogIn();
 });
