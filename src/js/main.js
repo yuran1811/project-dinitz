661d2ea79e6617278e4013aa7df2d08c6d2e9689
yuran1811 <trieuvanbd123@gmail.com>
2021-11-12 07:13:11 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,21 @@ const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
 
 sign_up_btn.addEventListener('click', () => {
-  container.classList.add('sign-up-mode');
+	container.classList.add('sign-up-mode');
 });
 
 function LogIn() {
-  const userName = document.querySelector('.user-name').value;
-  const Password = document.querySelector('.password').value;
-  if (userName !== 'Admin' || Password !== '1234') return;
-  // alert('You are not Admin, please try again!');
-  else {
-    alert('Log In Successful');
-    sign_up_btn.classList.add('active');
-    $('#sign-in-btn a').attr('href', 'src/Test_Template.html');
-  }
+	const userName = document.querySelector('.user-name').value;
+	const Password = document.querySelector('.password').value;
+	if (userName !== 'Admin' || Password !== '1234') return;
+	else {
+		sign_up_btn.classList.add('active');
+		$('#sign-in-btn a').attr('href', 'src/Test_Template.html');
+	}
 }
 
 document.querySelector('.log-in').addEventListener('click', LogIn());
 
 $('.input-field').on('keydown', function (event) {
-  if (event.keyCode === 13) LogIn();
+	LogIn();
 });
