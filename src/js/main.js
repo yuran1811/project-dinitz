ed8adc5ffdf5c294e42e0327d35e85dfe3faf298
yuran1811 <trieuvanbd123@gmail.com>
2021-11-18 17:01:32 +0700

    Fixed Login Button Bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,26 @@
-function logIn(e) {
+function check(e) {
 	const username = $('.user-name').val();
 	const password = $('.password').val();
-	if (e.keyCode === 13) {
-		if (username !== 'Admin' || password !== '1234') {
-			$('#log-in').val('Đăng nhập thất bại');
-			$('#log-in').removeClass('correct');
-			$('#log-in').addClass('wrong');
-			return;
-		}
-		$('#sign-in-btn a').attr('href', 'src/Test_Template.html');
-		$('#sign-up-btn').addClass('active');
-
-		$('#log-in').val('Đăng nhập thành công');
-		$('#log-in').removeClass('wrong');
-		$('#log-in').addClass('correct');
+	if (username !== 'Admin' || password !== '1234') {
+		$('#log-in').val('Đăng nhập thất bại');
+		$('#log-in').removeClass('correct');
+		$('#log-in').addClass('wrong');
+		return;
 	}
+	$('#sign-in-btn a').attr('href', 'src/Test_Template.html');
+	$('#sign-up-btn').addClass('active');
+
+	$('#log-in').val('Đăng nhập thành công');
+	$('#log-in').removeClass('wrong');
+	$('#log-in').addClass('correct');
 }
 
 $('.input-field').on('keydown', function (event) {
-	logIn(event);
+	if (event.keyCode === 13) check(event);
 });
 
 $('#log-in').on('click', function (event) {
-	logIn(event);
+	check(event);
 });
 
 $('#sign-up-btn').on('click', function () {
