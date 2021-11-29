1055cf2322d33f98c071de5e863c5c1565fec162
yuran1811 <trieuvanbd123@gmail.com>
2021-11-29 12:22:44 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,45 @@
-function check() {
-	const username = $('.user-name').val();
-	const password = $('.password').val();
+'use strict';
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+fetch('./db/acc.json')
+	.then(function (rsp) {
+		return rsp.json();
+	})
+	.then(function (data) {
+		console.log(data);
+	})
+	.catch(function (err) {
+		console.log(`Err: ${err}`);
+	});
+
+const check = () => {
+	const username = $('.user-name').value;
+	const password = $('.password').value;
+
 	if (username !== 'Admin' || password !== '1234') {
-		$('#log-in').val('Đăng nhập thất bại');
-		$('#log-in').removeClass('correct');
-		$('#log-in').addClass('wrong');
+		$('#log-in').value = 'Đăng nhập thất bại';
+		$('#log-in').classList.remove('correct');
+		$('#log-in').classList.add('wrong');
 		return;
 	}
-	$('#sign-in-btn a').attr('href', 'src/Test_Template.html');
-	$('#sign-up-btn').addClass('active');
+	$('#sign-in-btn a').setAttribute('href', 'src/Test_Template.html');
+	$('#sign-up-btn').classList.add('active');
 
-	$('#log-in').val('Đăng nhập thành công');
-	$('#log-in').removeClass('wrong');
-	$('#log-in').addClass('correct');
-}
+	$('#log-in').value = 'Đăng nhập thành công';
+	$('#log-in').classList.remove('wrong');
+	$('#log-in').classList.add('correct');
+};
 
-$('.input-field').on('keydown', function (event) {
-	if (event.keyCode === 13) check(event);
+$$('.input-field').forEach((item) => {
+	item.addEventListener('keydown', (e) => {
+		if (e.keyCode === 13) check();
+	});
 });
 
-$('#log-in').on('click', function (event) {
-	check(event);
-});
+$('#log-in').addEventListener('click', check);
 
-$('#sign-up-btn').on('click', function () {
-	$('.container').addClass('sign-up-mode');
+$('#sign-up-btn').addEventListener('click', () => {
+	$('.container').classList.add('sign-up-mode');
 });
